9728bc0f15e1b0fa691bdde717b4afde8c732005
Margarita <87192791+maggloo@users.noreply.github.com>
2024-03-15 15:28:45 +0300

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,9 @@
+function twoSum(numbers, target) {
+   for (let i = 0; i < numbers.length; i++){
+      for (let j = 0; j < numbers.length; j++){   
+        if (numbers[i] + numbers[j] === target && i !== j){
+          return [i, j]
+        }
+      }
+    }
+}
